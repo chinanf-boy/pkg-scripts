43ac0c3e7afe0c6c9d5d10c121f4ed78b79a6c56
chinanf-boy <865501259@qq.com>
2018-05-28 14:49:06 +0800

    remove size-limit

--- cli.js
+++ cli.js
@@ -1,20 +1,82 @@
 #!/usr/bin/env node
+
 'use strict';
-const meow = require('meow');
-const pkgScripts = require('./index');
+const pkgScripts = require('.');
+const signale = require('signale');
 
-const cli = meow(`
-	Usage
-	  $ pkg-scripts
+(async function () {
+	let pObj = await pkgScripts()
+	let pArr = Object.keys(pObj)
 
-	Show package.json scripts
+	let options = {
+		types: {
+		  test: {
+			color: 'yellow',
+			label: 'test'
+		  },
+		  build: {
+			color: 'blue',
+			label: 'build'
+		  },
+		  start: {
+			color: 'redBright',
+			label: 'start'
+		  },
+		  dev: {
+			color: 'magenta',
+			label: 'dev'
+		  }
+		}
+	  };
 
-`);
+	const scripts = new signale.Signale(options)
 
-(async function(){
-	let p = await pkgScripts()
+	let pre = [] // precommit
+	let one = [] // build
+	let two = [] // signale default
+	let thr = [] // other
 
-	console.log(JSON.stringify(p,null,2));
+	let preOptions = {
+		types: {
+			pre: {
+			  badge: '...',
+			  color: 'yellow'
+			},
+		}
+	  };
+
+	const preScripts = new signale.Signale(preOptions)
+
+	pArr.forEach(name => {
+		if(name.startsWith('pre')){
+			pre.push(name)
+		}else if(scripts[name]){
+			one.push(name)
+		}else{
+			signale[name] ?
+			(two.push(name)) :
+			(thr.push(name))
+		}
+	})
+	const log  = console.log.bind(console)
+	log()
+
+	pre.length && pre.forEach(name =>{
+		preScripts._scopeName = name
+		preScripts['pre'](pObj[name])
+	})
+	log()
+
+	one.length && one.forEach(name =>scripts[name](pObj[name]))
+	log()
+
+	two.length && two.forEach(name =>signale[name](pObj[name]))
+	log()
+
+	thr.length && thr.forEach(name =>signale._log(name+": "+pObj[name]))
+
+
+
+
+
 })()
-
-
