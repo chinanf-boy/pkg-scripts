e9504d6c9ff84e5d1faed7e57be07876078dba03
chinanf-boy <865501259@qq.com>
2018-06-04 11:25:45 +0800

    Fix test

--- test.js
+++ test.js
@@ -4,11 +4,11 @@ import m,{sync} from './index';
 test('async', async t => {
 	let res = await m()
 	let l = Object.keys(res).length
-	t.is(l, 9)
+	t.is(l, 10)
 });
 
 test('sync', async t => {
 	let res = sync()
 	let l = Object.keys(res).length
-	t.is(l, 9)
+	t.is(l, 10)
 });
